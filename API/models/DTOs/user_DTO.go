15659c3bac74c758e6fc75a89652f13c5032ff52
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2025-04-03 11:29:33 -0400

    Ajout du middleware pour la vérification du token et simplification d… (#66)
    * Ajout du middleware pour la vérification du token et simplification de auth_controller
    
    * WIP
    
    * WIP
    
    * updated deploy and tests
    
    * Extracted the logic in services
    
    * added configs in deployments
    
    * fixed 401 redirect
    
    * fixed code based on pr review
    
    ---------
    
    Co-authored-by: Antoine Chagnon-Michaud <antchmcg@cegeprdl.ca>
    Co-authored-by: antoine chagnon michaud <antoine.chagnonmichaud@cegeprdl.ca>

---
+++ API/models/DTOs/user_DTO.go
@@ -0,0 +1,11 @@
+package DTOs
+
+import "llio-api/models/enums"
+
+type User struct {
+	Id        int            `json:"id"`
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Email     string         `json:"email"`
+	Role      enums.UserRole `json:"role"`
+}
---
+++ API/services/auth_service.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"errors"
+	"llio-api/models/DTOs"
+	"llio-api/models/enums"
+	"llio-api/useful"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func ValidateTokenAndExtractUser(accessToken string) (*DTOs.User, error) {
+	useful.LoadEnv()
+
+	// Validate the token and extract claims
+
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract claims from the token
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("token invalide")
+	}
+
+	user := &DTOs.User{
+		Email:     claims["email"].(string),
+		FirstName: claims["first_name"].(string),
+		LastName:  claims["last_name"].(string),
+		Role:      enums.UserRole(claims["role"].(float64)),
+	}
+	return user, nil
+}
+
+func CreateJWTToken(userEmail string, fisrtName string, lastName string, expiresAt time.Time, userRole enums.UserRole) (string, error) {
+	// Define the claims for the token
+	secretKey := os.Getenv("JWT_SECRET_KEY")
+	claims := jwt.MapClaims{
+		"email":      userEmail,
+		"first_name": fisrtName,
+		"last_name":  lastName,
+		"exp":        expiresAt.Unix(),
+		"role":       userRole, // TODO aller chercher le role dans la base de données et le passer en paramètre ici
+	}
+
+	// Create a new token with the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with the secret key
+	signedToken, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
