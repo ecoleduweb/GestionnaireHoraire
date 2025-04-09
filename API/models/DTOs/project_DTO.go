3c16fcfba5b59390a5216319a6bae2844ad61711
Quentin Lecourt <quentin.lecourt@protonmail.com>
2025-04-09 14:00:32 -0400

    Commentaire ajouté

--- API/models/DTOs/project_DTO.go
+++ API/models/DTOs/project_DTO.go
@@ -6,7 +6,7 @@ type ProjectDTO struct {
 	Id          int           `json:"id"`
 	Name        string        `json:"name" binding:"required,max=50"`
 	Description string        `json:"description" binding:"required"`
-	Status      bool          `json:"status"`
+	Status      bool          `json:"status"` // Status du projet( si le projet est en cours ou fini)
 	Activities  []ActivityDTO `json:"activities"`
 	Categories  []CategoryDTO `json:"categories"`
 	CreatedAt   time.Time     `json:"created_at"`
