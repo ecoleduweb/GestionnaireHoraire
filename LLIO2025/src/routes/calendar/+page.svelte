4f4b3ab40b63e6ab7e30f53417ee955d8309bbb3
JeanFelixLarouche <2238092@cegeprdl.ca>
2025-04-23 09:20:34 -0400

    Ajout des components de left pane

--- LLIO2025/src/routes/calendar/+page.svelte
+++ LLIO2025/src/routes/calendar/+page.svelte
@@ -3,7 +3,7 @@
   import { CalendarService as CS } from '../../services/calendar.service';
   import { onMount } from 'svelte';
   import ActivityModal from '../../Components/Calendar/ActivityModal.svelte';
-  import DashboardModal from '../../Components/Calendar/DashboardLeftPane.svelte';
+  import DashboardLeftPane from '../../Components/Calendar/DashboardLeftPane.svelte';
   import { ActivityApiService } from '../../services/ActivityApiService';
   import type { Activity } from '../../Models/index.ts';
   // Importez le fichier CSS
@@ -437,7 +437,7 @@
   </div>
 </div>
 
-<DashboardModal />
+<DashboardLeftPane />
 
 <!-- Modal d'activité qui s'affiche par-dessus tout le reste -->
 {#if showModal}
