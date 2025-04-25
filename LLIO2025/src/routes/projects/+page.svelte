e6bf5c2d7df9543662fe411ad08fee9f87e14f90
JeanFelixLarouche <2238092@cegeprdl.ca>
2025-04-24 20:42:58 -0400

    Fix pour les tests, ajout de mocks archivés

--- LLIO2025/src/routes/projects/+page.svelte
+++ LLIO2025/src/routes/projects/+page.svelte
@@ -23,6 +23,7 @@
       name: "Nommer le projet",
       color: "blue",
       lead: "Katell Arnault de la Ménardière",
+      isArchived: false,
       coLeads: ["Jean-François Jasmin"],
       employees: [
         {
@@ -50,6 +51,7 @@
       name: "Le projet de Marie Amélie",
       color: "pink",
       lead: "Katell Arnault de la Ménardière",
+      isArchived: false,
       coLeads: ["Marie Amélie Dubé", "Jimmy Paquet-Cormier"],
       employees: [
         {
@@ -84,6 +86,7 @@
       name: "Graphisme 101",
       color: "yellow",
       lead: "Katell Arnault de la Ménardière",
+      isArchived: false,
       coLeads: [],
       employees: [
         {
@@ -106,6 +109,7 @@
       name: "Noisette Steve",
       color: "red",
       lead: "Steve Joncoux",
+      isArchived: false,
       coLeads: ["Katell Arnault de la Ménardière", "Marjolaine Poirier", "Jimmy Paquet-Cormier"],
       employees: [
         {
@@ -140,7 +144,134 @@
         }
       ],
       totalTimeSpent: 55
-      }
+      },
+      {
+      id: "AT-123",
+      name: "Nommer le projet",
+      color: "blue",
+      lead: "Katell Arnault de la Ménardière",
+      isArchived: true,
+      coLeads: ["Jean-François Jasmin"],
+      employees: [
+        {
+        name: "Katell Arnault de la Ménardière",
+        categories: [
+          { name: "Développement", timeSpent: 30, timeEstimated: 25 },
+          { name: "Graphisme", timeSpent: 15, timeEstimated: 30 }
+        ]
+        },
+        {
+        name: "Jean-François Jasmin",
+        categories: [
+          { name: "Développement", timeSpent: 20, timeEstimated: 50 }
+        ]
+        },
+        {
+        name: "Laure Desjardins",
+        categories: []
+        }
+      ],
+      totalTimeSpent: 65
+      },
+      {
+      id: "AT-456",
+      name: "Le projet de Marie Amélie",
+      color: "pink",
+      lead: "Katell Arnault de la Ménardière",
+      isArchived: true,
+      coLeads: ["Marie Amélie Dubé", "Jimmy Paquet-Cormier"],
+      employees: [
+        {
+        name: "Katell Arnault de la Ménardière",
+        categories: [
+          { name: "Développement", timeSpent: 12, timeEstimated: 15 }
+        ]
+        },
+        {
+        name: "Marie Amélie Dubé",
+        categories: [
+          { name: "Graphisme", timeSpent: 18, timeEstimated: 20 }
+        ]
+        },
+        {
+        name: "Ariane Dionne-Santerre",
+        categories: [
+          { name: "Rédaction", timeSpent: 8, timeEstimated: 10 }
+        ]
+        },
+        {
+        name: "Jimmy Paquet-Cormier",
+        categories: [
+          { name: "Développement", timeSpent: 14, timeEstimated: 15 }
+        ]
+        }
+      ],
+      totalTimeSpent: 52
+      },
+      {
+      id: "FO-115",
+      name: "Graphisme 101",
+      color: "yellow",
+      lead: "Katell Arnault de la Ménardière",
+      isArchived: true,
+      coLeads: [],
+      employees: [
+        {
+        name: "Katell Arnault de la Ménardière",
+        categories: [
+          { name: "Gestion", timeSpent: 5, timeEstimated: 8 }
+        ]
+        },
+        {
+        name: "Annie Côté",
+        categories: [
+          { name: "Graphisme", timeSpent: 22, timeEstimated: 25 }
+        ]
+        }
+      ],
+      totalTimeSpent: 27
+      },
+      {
+      id: "RA-224",
+      name: "Noisette Steve",
+      color: "red",
+      lead: "Steve Joncoux",
+      isArchived: true,
+      coLeads: ["Katell Arnault de la Ménardière", "Marjolaine Poirier", "Jimmy Paquet-Cormier"],
+      employees: [
+        {
+        name: "Steve Joncoux",
+        categories: [
+          { name: "Développement", timeSpent: 16, timeEstimated: 20 }
+        ]
+        },
+        {
+        name: "Katell Arnault de la Ménardière",
+        categories: [
+          { name: "Gestion", timeSpent: 7, timeEstimated: 10 }
+        ]
+        },
+        {
+        name: "Marjolaine Poirier",
+        categories: [
+          { name: "Rédaction", timeSpent: 12, timeEstimated: 15 }
+        ]
+        },
+        {
+        name: "Jimmy Paquet-Cormier",
+        categories: [
+          { name: "Développement", timeSpent: 9, timeEstimated: 12 }
+        ]
+        },
+        {
+        name: "Marie Amélie Dubé",
+        categories: [
+          { name: "Graphisme", timeSpent: 11, timeEstimated: 10 }
+        ]
+        }
+      ],
+      totalTimeSpent: 55
+      },
     ];
   }
 
