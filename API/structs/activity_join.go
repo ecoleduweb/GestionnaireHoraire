9d430b4c952fd7413ddc0cbd826bce8e531306b3
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2025-04-30 10:40:59 -0400

    merged

--- API/structs/activity_join.go
+++ API/structs/activity_join.go
@@ -1,11 +1,5 @@
 package structs
 
-import (
-	"llio-api/models/DAOs"
-)
-
-// Structure temporaire pour recevoir les résultats du JOIN
 type ActivityJoin struct {
-	DAOs.Activity
-	ProjectName string 
+	
 }
