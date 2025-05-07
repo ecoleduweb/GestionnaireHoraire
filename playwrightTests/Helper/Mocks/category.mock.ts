e6028472adc47ec0edab201742f2bc85e079518a
JeremieLapointe13 <2141363@cegeprdl.ca>
2025-05-07 15:26:36 -0400

    pour antoine

--- playwrightTests/Helper/Mocks/category.mock.ts
+++ playwrightTests/Helper/Mocks/category.mock.ts
@@ -1,14 +1,26 @@
 import { MockConfig } from "../types";
 
 export const categoryMocks = {
-    getCategoriesByProjectSuccess: {
-        url: '*/**/project/*/categories',
-        method: 'GET',
-        response: {
-            status: 200,
-            json: {
-                "categories": []
-            }
-        }
-    }
+  getCategoriesByProjectSuccess: {
+    url: "*/**/project/*/categories",
+    method: "GET",
+    response: {
+      status: 200,
+      json: {
+        categories: [
+          {
+            id: 1,
+            name: "Test",
+            description: "Nouveau format",
+            billable: false,
+            activities: [],
+            createdAt: "2025-04-02T10:29:54-04:00",
+            updatedAt: "2025-04-02T10:29:54-04:00",
+            userId: 1,
+            projectId: 1,
+          },
+        ],
+      },
+    },
+  },
 } satisfies Record<string, MockConfig>;
