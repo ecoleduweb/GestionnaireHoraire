1fb234af2d74e2d16946f2aeeeaf23c12dac64ff
VincentBouchar-d <2236622@cegeprdl.ca>
2025-06-18 16:23:36 -0400

    Test pour un logout avec succès

---
+++ playwrightTests/tests/test.logOut.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+import { ApiMocker } from '../Helper/mockApi';
+import { projectMocks } from '../Helper/Mocks/project.mock';
+import { userMocks } from '../Helper/Mocks/user.Mock';
+import { activityMocks } from '../Helper/Mocks/activity.mock';
+
+test.describe('logOut', () => {
+
+    test.beforeEach(async ({ page }) => {
+        const apiMocker = new ApiMocker(page);
+        await apiMocker.addMocks([
+            activityMocks.getAllActivitiesDefaultWeekSuccess,
+            userMocks.userMeSuccess
+        ])
+            .apply();
+        await page.clock.install({ time: new Date('2025-03-22T08:00:00') });
+        await page.goto('http://localhost:5002/calendar');
+        await page.waitForLoadState('networkidle');
+    });
+
+    test('logoutSucces', async ({ page }) => {
+        await page.getByTitle('Se déconnecter').click();
+        await expect(page.getByText('Se connecter avec Microsoft')).toBeVisible();
+
+    });
+});
