da713a785d6c51fb2dee833f0b0b5460576e95a0
William Lévesque <2343341@cegeprdl.ca>
2026-05-28 04:33:51 -0400

    feat: Ajout du statut du projet à l'export Excel

--- API/services/report_service.go
+++ API/services/report_service.go
@@ -3,25 +3,26 @@ package services
 import (
 	"fmt"
 	"io"
+	"llio-api/models/enums"
 	"llio-api/repositories"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func GenerateExcel(w io.Writer) error {
-    activities, err := repositories.GetAllForExport()
-    if err != nil {
-        return fmt.Errorf("failed to fetch activities: %w", err)
-    }
+	activities, err := repositories.GetAllForExport()
+	if err != nil {
+		return fmt.Errorf("failed to fetch activities: %w", err)
+	}
 
 	f := excelize.NewFile()
 	sheet := "Sheet1"
 
 	headers := []string{
 		"Prénom", "Nom",
-		"Projet", "Unique ID",
+		"Projet", "Unique ID", "Statut du projet",
 		"Activité", "Catégorie",
-		"Date de début", "Date de fin","Temps passé (h)",
+		"Date de début", "Date de fin", "Temps passé (h)",
 	}
 
 	for i, h := range headers {
@@ -32,18 +33,24 @@ func GenerateExcel(w io.Writer) error {
 	row := 2
 
 	for _, a := range activities {
+		status := "actif"
+		if a.Project.Status == enums.ProjectStatus(enums.Archived) {
+			status = "archivé"
+		}
+
 		f.SetCellValue(sheet, fmt.Sprintf("A%d", row), a.User.FirstName)
 		f.SetCellValue(sheet, fmt.Sprintf("B%d", row), a.User.LastName)
 
 		f.SetCellValue(sheet, fmt.Sprintf("C%d", row), a.Project.Name)
 		f.SetCellValue(sheet, fmt.Sprintf("D%d", row), a.Project.UniqueId)
+		f.SetCellValue(sheet, fmt.Sprintf("E%d", row), status)
 
-		f.SetCellValue(sheet, fmt.Sprintf("E%d", row), a.Name)
-		f.SetCellValue(sheet, fmt.Sprintf("F%d", row), a.Category.Name)
+		f.SetCellValue(sheet, fmt.Sprintf("F%d", row), a.Name)
+		f.SetCellValue(sheet, fmt.Sprintf("G%d", row), a.Category.Name)
 
-		f.SetCellValue(sheet, fmt.Sprintf("G%d", row), a.StartDate.Format("2006-01-02 15:04"))
-		f.SetCellValue(sheet, fmt.Sprintf("H%d", row), a.EndDate.Format("2006-01-02 15:04"))
-        f.SetCellValue(sheet, fmt.Sprintf("I%d", row), a.TimeSpent)
+		f.SetCellValue(sheet, fmt.Sprintf("H%d", row), a.StartDate.Format("2006-01-02 15:04"))
+		f.SetCellValue(sheet, fmt.Sprintf("I%d", row), a.EndDate.Format("2006-01-02 15:04"))
+		f.SetCellValue(sheet, fmt.Sprintf("J%d", row), a.TimeSpent)
 
 		row++
 	}
