cdb078abe5d42e0db6569143db7e9d987b553650
William Lévesque <157745218+levwil@users.noreply.github.com>
2026-05-28 19:57:20 -0400

    Fix du problème de fuseaux horaires à l'import Outlook (#339)
    * feat: ajout d'un convertisseur heure locale à UTC
    
    * fix: conversion du temps local en UTC avant le de passer à Graph API

--- API/useful/date_utils.go
+++ API/useful/date_utils.go
@@ -1,6 +1,9 @@
 package useful
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // DateToISOString Formatter la date dans le format RFC3339 (YYYY-MM-DDThh:mm:ssZ), notamment utile pour l'API Outlook.
 func DateToISOString(date time.Time) string {
@@ -13,4 +16,24 @@ func ToStartOfDay(date time.Time) time.Time {
 
 func ToEndOfDay(date time.Time) time.Time {
 	return time.Date(date.Year(), date.Month(), date.Day(), 23, 59, 59, 0, date.Location())
+}
+
+// AsTorontoThenUTC prends une heure existante, peu importe son fuseau horaire, et la considère comme étant du fuseau
+// horaire local. Elle la reconverti alors en heure UTC, ce qui peut être utile pour certains services externes comme
+// Graph API.
+func AsTorontoThenUTC(date time.Time) (time.Time, error) {
+	loc, err := time.LoadLocation("America/Toronto")
+	if err != nil {
+		return time.Time{}, fmt.Errorf("failed to load timezone: %w", err)
+	}
+
+	// Reprends les mêmes chiffres que la date originale, mais lui applique le fuseau horaire America/Toronto plutôt que
+	// celui existant/celui du serveur
+	toronto := time.Date(
+		date.Year(), date.Month(), date.Day(),
+		date.Hour(), date.Minute(), date.Second(), date.Nanosecond(),
+		loc,
+	)
+
+	return toronto.UTC(), nil
 }
