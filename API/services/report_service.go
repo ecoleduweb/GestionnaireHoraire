f8f9813c8824d1d8f4a4164492da12290a6ed98b
William Lévesque <2343341@cegeprdl.ca>
2026-05-28 04:40:51 -0400

    fix: Conversion du temps en heure locale à l'export Excel

--- API/services/report_service.go
+++ API/services/report_service.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"llio-api/models/enums"
 	"llio-api/repositories"
+	"time"
 
 	"github.com/xuri/excelize/v2"
 )
@@ -13,6 +14,11 @@ func GenerateExcel(w io.Writer) error {
 	activities, err := repositories.GetAllForExport()
 	if err != nil {
 		return fmt.Errorf("failed to fetch activities: %w", err)
+	}
+
+	loc, err := time.LoadLocation("America/Toronto")
+	if err != nil {
+		return fmt.Errorf("failed to load timezone: %w", err)
 	}
 
 	f := excelize.NewFile()
@@ -48,8 +54,8 @@ func GenerateExcel(w io.Writer) error {
 		f.SetCellValue(sheet, fmt.Sprintf("F%d", row), a.Name)
 		f.SetCellValue(sheet, fmt.Sprintf("G%d", row), a.Category.Name)
 
-		f.SetCellValue(sheet, fmt.Sprintf("H%d", row), a.StartDate.Format("2006-01-02 15:04"))
-		f.SetCellValue(sheet, fmt.Sprintf("I%d", row), a.EndDate.Format("2006-01-02 15:04"))
+		f.SetCellValue(sheet, fmt.Sprintf("H%d", row), a.StartDate.In(loc).Format("2006-01-02 15:04"))
+		f.SetCellValue(sheet, fmt.Sprintf("I%d", row), a.EndDate.In(loc).Format("2006-01-02 15:04"))
 		f.SetCellValue(sheet, fmt.Sprintf("J%d", row), a.TimeSpent)
 
 		row++
